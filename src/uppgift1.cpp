22dc14f2e7450ef1fadfa2d7042366396e389f19
bergstromkasper <18kaberg@mediagymnasiet.se>
2019-01-28 14:13:59 +0100

    Uppgift 1

---
+++ src/uppgift1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int today, lastyear, mil;
+
+ cout << "Hur många mil har din bil kört idag? " << endl;
+ cin >> today;
+ cout << "Hur många mil hade din bil kört för ett år sedan? " << endl;
+ cin >> lastyear;
+ mil = today - lastyear;
+ cout << "Din bil har kört " << mil << " mil";
+}
