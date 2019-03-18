6309d8b6fd8d571b135145f5c6b1eab4a64b70c2
Kasper Bergström <18kaberg@mg.lan>
2019-03-18 13:56:53 +0100

    Uppgift 22 - arrays 1

---
+++ Uppgift22/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+int array[10] = {1,2,3,4,5,6,7,8,9,10};
+
+for(int i = 0; i < 10; i++){
+	cout << array[i] << endl;
+}
+	return 0;
+}
